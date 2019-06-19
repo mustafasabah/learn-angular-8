d2003de6861298eb50be5d407ab48b0017fbf682
M-SABAH <mustafasabah123123@gmail.com>
2019-06-19 06:40:13 +0200

    create some components like nav, home, about, contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
